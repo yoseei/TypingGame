73cc87314f5546728abacdca96b5dd6d52371a1a
甲斐義隆 <katashiyoika@mba.local>
2021-03-02 22:52:21 +0900

    マウスストーカー追尾も完成

--- js/main.js
+++ js/main.js
@@ -3,6 +3,34 @@
 {
   const stalker = document.getElementById('stalker'); 
 
+  let hovFlag = false;
+
+  document.addEventListener('mousemove', function (e) {
+    if(!hovFlag) {
+      stalker.style.transform = 'translate(' + e.clientX + 'px, ' + e.clientY + 'px)';
+    }
+  });
+
+  const linkElem = document.querySelectorAll('a:not(.no_stick_)');
+  for (let i = 0; i < linkElem.length; i++) {
+    linkElem[i].addEventListener('mouseover', function (e) {
+      hovFlag = true;
+
+      stalker.classList.add('hov_');
+
+      let rect = e.target.getBoundingClientRect();
+      let posX = rect.left + (rect.width / 2);
+      let posY = rect.top + (rect.height / 2);
+
+      stalker.style.transform = 'translate(' + posX + 'px, ' + posY + 'px)';
+
+    });
+    linkElem[i].addEventListener('mouseout', function (e) {
+      hovFlag = false;
+      stalker.classList.remove('hov_');
+    });
+  }
+
   //上記のdivタグをマウスに追従させる処理
   document.addEventListener('mousemove', function (e) {
     stalker.style.transform = 'translate(' + e.clientX + 'px, ' + e.clientY + 'px)';
