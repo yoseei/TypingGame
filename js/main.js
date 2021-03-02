7fd020d5842c0499e70335ec77bd1eb0abda3000
甲斐義隆 <katashiyoika@mba.local>
2021-03-02 10:58:23 +0900

    add reload text

--- js/main.js
+++ js/main.js
@@ -1,51 +1,7 @@
 'use strict';
 
 {
-  // function setWord() {
-  //   word = words.splice(Math.floor(Math.random() * words.length), 1)[0];
-  //   target.textContent = word;
-  //   loc = 0;
-  // }
-  // const words = [
-  //   'red',
-  //   'blue',
-  //   'yellow',
-  // ]
-
-  // let word;
-  // let loc = 0;
-  // let startTime;
-  // let isPlaying = false;
-  // const target = document.getElementById('target');
-
-  // document.addEventListener('click', () => {
-  //   if (isPlaying === true) {
-  //     return;
-  //   }
-  //   isPlaying = true;
-  //   startTime = Date.now();
-  //   setWord();
-  // });
-
-  // document.addEventListener('keydown', l => {
-  //   if (l.key !== word[loc]) {
-  //     return;
-  //   }
-  //   loc++;
-  //   target.textContent = '_'.repeat(loc) + word.substring(loc);  
-    
-  //   if (loc === word.length) {
-  //     if (words.length === 0) {
-  //       let elapsedTime = ((Date.now() - startTime) / 1000).toFixed(2);
-  //       const result = document.getElementById('result');
-  //       result.textContent = `Finished! ${elapsedTime} seconds!`;
-  //       return;
-  //     }
-  //     setWord();
-  //   } 
-  // });
-
-
+  
   function setWord() {
     word = words.splice(Math.floor(Math.random() * words.length), 1)[0];
     target.textContent = word;
@@ -91,13 +47,18 @@
       if (words.length === 0) {
         const elapsedTime = ((Date.now() - startTime) / 1000).toFixed(2);
         const result = document.getElementById('result');
+        let reload = document.getElementById('reload');
+        reload.textContent = 'Try Again! Please Refresh This Page!'
+
         if (elapsedTime <= 13) {
-          result.textContent = `${elapsedTime} 秒！
-          君が天下の大将軍だ！！`;
+          result.textContent = `${elapsedTime} 秒！\n君が天下の大将軍だ！！`;
+          `${reload}`;
         } else if (elapsedTime <= 15) {
           result.textContent = `${elapsedTime}秒！\n1000人将に任命だ！！`;
+          `${reload}`;
         } else {
           result.textContent = `${elapsedTime}秒！\n歩兵から出直して来い！`
+          `${reload}`;
           return;
         }
       }
