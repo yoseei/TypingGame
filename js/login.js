6769d4205b4b2ebdafee131279e0dd58b4b49a6d
甲斐義隆 <katashiyoika@mba.local>
2021-03-08 14:45:45 +0900

    アラートメッセージ再度修正

--- js/login.js
+++ js/login.js
@@ -19,7 +19,7 @@ function signUp() {
     const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
   
     promise.catch(_ => alert("エラーです。\nメールアドレスを正しく入力してください。\nまた、パスワードは６文字以上で設定してください。"));
-    promise.then("新規登録が完了しました。");
+    promise.then(_ => alert("新規登録が完了しました。"));
   } else {
     alert("メールアドレスとパスワードは必須です。");
   }
@@ -30,8 +30,9 @@ function signIn() {
   var password = document.getElementById("password");
 
   const promise = auth.signInWithEmailAndPassword(email.value, password.value);
-  alert("ログインしました。");
+
   promise.catch(_ => alert("メールアドレス、またはパスワードが間違っています。"));
+  promise.then(_ => alert("ログインしました。"));
 }
 
 function signOut() {
@@ -42,7 +43,7 @@ function signOut() {
 auth.onAuthStateChanged(function(user) {
   if(user) {
     var email = user.email;
-    const ("ログインユーザー：" + email);
+    alert("ログインユーザー：" + email);
   } else {
     alert("ログイン、または新規登録をしてください。");
   }
